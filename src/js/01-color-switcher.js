ff8f4150ca13f0fbb12d54def5f27a71c1249ce9
Valentina-K <kvalentyna@hotmail.com>
2022-12-05 22:31:29 +0200

    task-01 start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const refs = {
+    bodyRef: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]')
+}
 
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+function onStart() {
+    
+}
+
+function onStop() {
+    
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
