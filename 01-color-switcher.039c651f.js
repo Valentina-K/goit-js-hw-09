edf82b6227165f87d7e21820c9f6a83845ac2397
Valentina-K <kvalentyna@hotmail.com>
2022-12-06 10:08:58 +0000

    Deploying to gh-pages from @ Valentina-K/goit-js-hw-09@1360544b2609b7622d2993380bae400ccc38d228 🚀

--- 01-color-switcher.039c651f.js
+++
@@ -1,2 +0,0 @@
-const t={bodyRef:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(function(){})),t.btnStop.addEventListener("click",(function(){}));
-//# sourceMappingURL=01-color-switcher.039c651f.js.map
--- 01-color-switcher.e040aba6.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={bodyRef:document.querySelector("body"),btnStart:document.querySelector("[data-start]"),btnStop:document.querySelector("[data-stop]")};t.btnStart.addEventListener("click",(function(){})),t.btnStop.addEventListener("click",(function(){}))}();
-//# sourceMappingURL=01-color-switcher.e040aba6.js.map
