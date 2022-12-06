1360544b2609b7622d2993380bae400ccc38d228
Valentina-K <kvalentyna@hotmail.com>
2022-12-06 12:08:26 +0200

    task-01 is ok

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,41 @@
+class TimerColor{
+  constructor({onTick}) {
+    this.intervalId = null;
+    this.isStarted = false;
+    this.onTick = onTick;
+  }
+
+  start() {
+    if (this.isStarted) {
+      return;
+    }
+    this.isStarted = true;
+    this.intervalId = setInterval(() => {
+      this.onTick();
+    }, 1000);    
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.isStarted = false;
+  }  
+}
+
 const refs = {
     bodyRef: document.querySelector('body'),
     btnStart: document.querySelector('[data-start]'),
     btnStop: document.querySelector('[data-stop]')
 }
 
-refs.btnStart.addEventListener('click', onStart);
-refs.btnStop.addEventListener('click', onStop);
+const timerColor = new TimerColor({
+  onTick: updateColor
+});
 
-function onStart() {
-    
-}
+refs.btnStart.addEventListener('click', () => timerColor.start());
+refs.btnStop.addEventListener('click', () => timerColor.stop());
 
-function onStop() {
-    
+function updateColor() {
+  refs.bodyRef.setAttribute('style', `background-color: ${getRandomHexColor()}`);
 }
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
